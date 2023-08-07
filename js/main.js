032a541502033f6aca88ed8845c876ffaa8e8c10
Motoakist <45988963+Motoakist@users.noreply.github.com>
2023-08-08 05:05:40 +0900

    draggable false

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ const modelPath = "https://cdn.jsdelivr.net/gh/edu-053/AgentInteractionResources
 const requiredResources = [
     "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js",
     "https://cdn.jsdelivr.net/gh/dylanNew/live2d/webgl/Live2D/lib/live2d.min.js",
-    "https://cdn.jsdelivr.net/gh/Motoakist/humanlike_agent@8b78d36667a4bf2dc354edb306d48eacf525246b/indexLibrary/indexLibrary.js",
+    "https://cdn.jsdelivr.net/gh/Motoakist/humanlike_agent@e033a1a977b279c049a19a1ed33bc5dabb986f18/indexLibrary/indexLibrary.js",
 
 ];
 
