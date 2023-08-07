4e90f62bd2f4fd4cdddb4fa37e97d088a50b904a
Motoakist <45988963+Motoakist@users.noreply.github.com>
2023-08-07 16:08:47 +0900

    model size

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const position = {
-    boxWidth: 4000,
+    boxWidth: 12000,
     // boxHeight: 700,
-    boxHeight: 4000,
+    boxHeight: 12000,
     // modelScale: 0.45,
     modelScale: 1,
     modelX: 400,
     // modelY: 500,
-    modelY: 7000,
+    modelY: 2000,
 };
 
 const serverURL = "https://a48e-2400-2651-41c2-1500-4405-5e59-5c98-3b57.jp.ngrok.io";
@@ -16,7 +16,7 @@ const modelPath = "https://cdn.jsdelivr.net/gh/edu-053/AgentInteractionResources
 const requiredResources = [
     "https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js",
     "https://cdn.jsdelivr.net/gh/dylanNew/live2d/webgl/Live2D/lib/live2d.min.js",
-    "https://cdn.jsdelivr.net/gh/Motoakist/humanlike_agent@62debd68f68eddcdb326e7e8093509f1e389bf29/indexLibrary/indexLibrary.js",
+    "https://cdn.jsdelivr.net/gh/Motoakist/humanlike_agent@c89f7b20fd076328a8a4e73492b1412483f6d4f4/indexLibrary/indexLibrary.js",
 
 ];
 
